a7cbd38050667b9a0ce998e6cdfcd7141ce2be8d
Gaurav Yadav <gaurav.yadav@connectrpl.com>
2024-04-24 12:48:03 +0530

    homepage styling

--- src/components/SideBarNew.jsx
+++ src/components/SideBarNew.jsx
@@ -16,20 +16,26 @@ export default function Sidebar({ children, setExpandedMain }) {
       {/* Sidebar for large devices */}
       <aside
         id="logo-sidebar"
-        className={`fixed top-0  hidden sm:block left-0 z-40 w-64 pt-0 h-screen sm:translate-x-0 transition-all bg-orange-900 ${
+        className={`fixed top-0  hidden sm:block left-0 z-40 w-64 pt-0 h-screen sm:translate-x-0 transition-all  ${
           expanded ? "w-[18%]  hidden sm:block" : "w-[5%] hidden sm:block"
         }`}
       >
-        <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+        <nav
+          className="h-full flex flex-col  shadow-sm"
+          style={{
+            background:
+              "linear-gradient(to bottom, rgba(255, 255, 255, 1) 0%, rgba(218, 236, 247, 0.8) 48.5%, rgba(255, 255, 255, 0.6) 98%)",
+          }}
+        >
           <div className="p-4 pb-2 flex justify-between items-center">
             <img // eslint-disable-line
-              src="https://storage.googleapis.com/connectrpl-landing-pagev3/siteimages/logonew.png"
+              src="https://storage.googleapis.com/connectrpl_images/crpl%20logo%203.svg"
               className={`overflow-hidden transition-all ${
-                expanded ? "w-28" : "w-0"
+                expanded ? "w-44" : "w-0"
               }`}
               alt=""
             />
-            <button
+            {/* <button
               onClick={() => {
                 setExpandedMain((curr) => !curr);
                 setExpanded((curr) => !curr);
@@ -37,11 +43,11 @@ export default function Sidebar({ children, setExpandedMain }) {
               className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
             >
               {expanded ? <ChevronFirst /> : <ChevronLast />}
-            </button>
+            </button> */}
           </div>
 
           <SidebarContext.Provider value={{ expanded }}>
-            <ul className="flex-1 px-3">{children}</ul>
+            <ul className="flex-1 px-3 ml-6 mt-4">{children}</ul>
           </SidebarContext.Provider>
 
           <div className="border-t flex p-3">
@@ -81,17 +87,19 @@ export function SidebarItem({ icon, text, active, alert, href }) {
         relative flex items-center py-2 px-3 my-1
         font-medium rounded-md cursor-pointer
         transition-colors group
-        ${
-          active
-            ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
-            : "hover:bg-indigo-50 text-gray-600"
-        }
+      
       `}
+
+        // ${
+        //   active
+        //     ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+        //     : "hover:bg-indigo-50 text-gray-600"
+        // }
       >
         {icon}
         <span
           className={`overflow-hidden transition-all ${
-            expanded ? "w-52 ml-3" : "w-0"
+            expanded ? "w-52 ml-8 text-lg" : "w-0"
           }`}
         >
           {text}
